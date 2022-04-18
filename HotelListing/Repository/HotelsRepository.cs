d0368c6ba48f28debabac03fb0a11004b46017fd
Haron Wanyoike <hwanyoike@abnosoftwares.com>
2022-04-18 20:24:33 +0300

    hotel repository

---
+++ HotelListing/Repository/HotelsRepository.cs
@@ -0,0 +1,15 @@
+﻿using HotelListing.Data;
+using HotelListing.IRepository;
+
+namespace HotelListing.Repository
+{
+
+    public class HotelsRepository :GenericRepository<Hotel>, IHotelsRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public HotelsRepository(ApplicationDbContext context): base(context)
+        {
+            _context = context;
+        }
+    }
+}
--- HotelListing/Startup.cs
+++ HotelListing/Startup.cs
@@ -38,6 +38,7 @@ namespace HotelListing
             services.AddAutoMapper(typeof(MapperConfig));
             //add repositories
             services.AddScoped<ICountriesRepository, CountriesRepository>();
+            services.AddScoped<IHotelsRepository, HotelsRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
